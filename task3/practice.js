01eab25bedac6ece4e41ce0eda17efe6afcb609c
Haneen Mohammad <haneenmoh775@gmail.com>
2026-07-07 15:42:20 +0300

    Add files via upload

---
+++ task3/practice.js
@@ -0,0 +1,18 @@
+function tellFortune(jobTitle, geographicLocation, partnerName, numberOfChildren) {
+
+    document.write("You will be a " + jobTitle +
+        " in " + geographicLocation +
+        ", and married to " + partnerName +
+        " with " + numberOfChildren + " kids.");
+
+}
+
+tellFortune("software engineer", "Jordan", "Alice", 3);
+function calculateDogAge()
+{
+let dogAge = age * 7;
+
+    document.write("Your doggie is " + dogAge + " years old in dog years!");
+
+}
+calculateDogAge(1);
